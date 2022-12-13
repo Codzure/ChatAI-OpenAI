2f52a6334d0057d767ae7bc53b43fab7ff99fed2
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-13 21:03:48 +0300

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Leonard Mutugi on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
